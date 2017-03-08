aa55ba22330452f05cc3d2ab996b226fbcab54ca
Neelesh bansal <Neelesh bansal>
2017-03-08 16:45:45 +0530

    Neighbourhood-map Added

---
+++ P7 (Neighborhood Map)/README.md
@@ -0,0 +1,26 @@
+# Neighbourhood-map
+
+
+How to open the Project 
+=======================
+* Download the Zip file and unzip it.
+* Open the index.html from the folder.
+* Run it.
+* Click on the markers you will findind rating and likes to the places.
+
+Things Used in the Project
+==========================
+* Google Maps Api
+* Knockout js
+* foursquare id
+* Bootstrap
+* Javascript
+* Jquery
+
+Uses of Project
+===============
+
+The Project is used to find the places where most of People visit and the things are more liked to see in the state and the locations where can the visit in the odisha.
+I have used the Foursquare Id to determine the Places which people like the most.
+
+                   *****************The END****************************
--- P7 (Neighborhood Map)/js/app.js
+++ P7 (Neighborhood Map)/js/app.js
@@ -1,147 +1,205 @@
-var locations=[
-      {
-			  title: 'Jagannath Temple',
-			 location:{
-				  lat: 19.8047181,
-          lng: 85.8179802,
-          id:'',
+//Model Starts here
+var locations = [{
+        title: 'Chung Wah Chinese Restaurant',
+        location: {
+            lat: 19.8011,
+            lng: 85.8319,
         },
-        show:true,
-        selected:false,
+        show: true,
+        selected: false,
+        id: '506ac48be4b0677a1fd4f871'
 
-		  },
-		  {
-			  title: 'Konark Sun Temple',
-			  location:{
-          lat: 19.8875953,
-          lng: 86.0945364,
+    },
+    {
+        title: 'Konark Sun Temple',
+        location: {
+            lat: 19.8875953,
+            lng: 86.0945364,
         },
-        show:true,
-        selected:false,
-        id:'4cbdd05b3481199c2ecc6d3f'
-		  },
-      {
-			  title: 'Chilika Lake',
-			  location:{
-          lat: 19.8450,
-         lng: 85.4788,
-       },
-       show:true,
-       selected:false,
-       id:''
-		  },
-		  {
-			  title: 'Puri Beach',
-			  location:{
-          lat: 19.7947,
-          lng: 85.8253,
+        show: true,
+        selected: false,
+        id: '4cbdd05b3481199c2ecc6d3f'
+    },
+    {
+        title: 'Hotel Holiday Resort',
+        location: {
+            lat: 19.8026,
+            lng: 85.8412,
         },
-        show:true,
-        selected:false,
-		    id:'4cfb649ac671721e4959c318'
-		  }
+        show: true,
+        selected: false,
+        id: '4bd79337304fce728db733ab'
+    },
+    {
+        title: 'Puri Beach',
+        location: {
+            lat: 19.7947,
+            lng: 85.8253,
+        },
+        show: true,
+        selected: false,
+        id: '4cfb649ac671721e4959c318'
+    },
+    {
+        title: 'Rajarani Temple',
+        location: {
+            lat: 20.2434,
+            lng: 85.8435,
+        },
+        show: true,
+        selected: false,
+        id: '4cfb649ac671721e4959c318'
+    }
+
+
 ];
 
+var markers = [];
+var viewModel = function() {
+    var self = this;
+    var largeInfowindow = new google.maps.InfoWindow();
+    for (var i = 0; i < locations.length; i++) {
+        var position = locations[i].location;
+        var title = locations[i].title;
+        var defaultIcon = makeMarkerIcon('0091ff');
+        var highlightedIcon = makeMarkerIcon('FFFF24');
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
+            show: ko.observable(locations[i].show),
+            selected: ko.observable(locations[i].selected),
+            venue: locations[i].id,
 
-var markers=[];
-var viewModel=function(){
-  var largeInfowindow = new google.maps.InfoWindow();
-for(var i=0;i<locations.length;i++)
-{
-  var position=locations[i].location;
-  var title=locations[i].title;
-  var defaultIcon = makeMarkerIcon('0091ff');
-  var highlightedIcon = makeMarkerIcon('FFFF24');
-  var marker=new google.maps.Marker({
-    map:map,
-    position:position,
-    title:title,
-    animation:google.maps.Animation.DROP,
-    icon:defaultIcon,
-	  show:ko.observable(locations[i].show),
-	  selected:ko.observable(locations[i].selected),
-	  venue:locations[i].id
-  })
-  markers.push(marker);
+        });
+        rating: '';
+        likes: '';
+        markers.push(marker);
 
 
-  //rating of the place
-  //color change on click
-  marker.addListener('click', function() {
-    populateInfoWindow(this, largeInfowindow);
-  });
-  marker.addListener('mouseover',function(){
-    this.setIcon(highlightedIcon);
-  });
+        //color change on click
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+        });
 
-  marker.addListener('mouseout',function(){
-    this.setIcon(defaultIcon);
-  });
-}
-function makeMarkerIcon(markerColor) {
-  var markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-    '|40|_|%E2%80%A2',
-    new google.maps.Size(21, 34),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(10, 34),
-    new google.maps.Size(21,34));
-  return markerImage;
-}
-function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    // Clear the infowindow content to give the streetview time to load.
-    infowindow.setContent('');
-    infowindow.marker = marker;
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
-      infowindow.marker = null;
-    });
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 50;
-    // In case the status is OK, which means the pano was found, compute the
-    // position of the streetview image, then calculate the heading, then get a
-    // panorama from that and set the options
-    function getStreetView(data, status) {
-      if (status == google.maps.StreetViewStatus.OK) {
-        var nearStreetViewLocation = data.location.latLng;
-        var heading = google.maps.geometry.spherical.computeHeading(
-          nearStreetViewLocation, marker.position);
-          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-          var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 30
-            }
-          };
-        var panorama = new google.maps.StreetViewPanorama(
-          document.getElementById('pano'), panoramaOptions);
-      } else {
-        infowindow.setContent('<div>' + marker.title + '</div>' +
-          '<div>No Street View Found</div>');
-      }
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+        });
+
+      marker.addListener('click',function(){
+        bounce(this);
+      });
     }
-    // Use streetview service to get the closest streetview image within
-    // 50 meters of the markers position
-    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-    // Open the infowindow on the correct marker.
-    infowindow.open(map, marker);
-  }
-}
-/*this.selectAll=function(marker){
-  this.selected=true;
-  populateInfoWindow(marker,largeInfowindow);
-};*/
-this.selectAll=function(marker){
-  marker.setAnimation(google.maps.Animation.BOUNCE);
-  marker.setIcon(highlightedIcon);
-  setTimeout(function() {
-            marker.setAnimation(null);
-            marker.setIcon(defaultIcon)
-        }, 700);
-  populateInfoWindow(marker,largeInfowindow);
-};
+    markers.forEach(function(marker) {
+        //console.log(marker);
+        $.ajax({
+            method: 'GET',
+            dataType: 'json',
+            url: 'https://api.foursquare.com/v2/venues/' + marker.venue + '?client_id=ZNBWSBOKCVOK4KML4XW0XVSSPXXGLVP3UEPOQVHE1T5YSB1E&client_secret=F2CLYORZCNBNNDIUVZATCZQW5DRUWFQHMZFTP1JMN3K3143I&v=20170305',
+            success: function(data) {
+                //console.log(marker.venue);
+                var request = data.response.venue;
+                if (request.hasOwnProperty('rating') != '') {
+                    marker.rating = request.rating;
+                } else {
+                    //marker.rating = '';
+                    marker.rating="Rating is not Found";
+                    //console.log("hello");
+                }
+                if (request.hasOwnProperty('likes') !='') {
+                    marker.likes = request.likes.summary;
 
-}
+                } else {
+                    marker.likes = 'Likes Not Found';
+
+                }
+
+            },
+            error: function(e) {
+                alert("Error loading in id");
+            }
+        });
+    });
+
+
+    function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+            '|40|_|%E2%80%A2',
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34),
+            new google.maps.Size(21, 34));
+        return markerImage;
+    }
+
+    function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '<br>' + marker.rating + '<br>' + marker.likes + '</div>');
+            infowindow.open(map, marker);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function() {
+                infowindow.marker = null;
+            });
+        }
+    }
+
+     function bounce(marker)
+     {
+       marker.setAnimation(google.maps.Animation.BOUNCE);
+       marker.setIcon(highlightedIcon);
+       setTimeout(function() {
+           marker.setAnimation(null);
+           marker.setIcon(defaultIcon);
+       }, 700);
+       populateInfoWindow(marker, largeInfowindow);
+     }
+    this.Bounce = function(marker) {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        marker.setIcon(highlightedIcon);
+        setTimeout(function() {
+            marker.setAnimation(null);
+            marker.setIcon(defaultIcon);
+        }, 700);
+        populateInfoWindow(marker, largeInfowindow);
+    };
+    this.query = ko.observable('');
+    //console.log(this.query);
+    this.filterList = function() {
+        var text = this.query();
+        largeInfowindow.close();
+        //here to close all the windows
+
+        if (text.length === 0) {
+            this.setAllShow(true);
+        } else {
+            for (var i = 0; i < markers.length; i++) {
+                // to check whether the searchText is there in the mapArray
+                if (markers[i].title.toLowerCase().indexOf(text.toLowerCase()) >= 0) {
+                    markers[i].show(true);
+                    markers[i].setVisible(true);
+                } else {
+                    markers[i].show(false);
+                    markers[i].setVisible(false);
+                }
+            }
+        }
+        largeInfowindow.close();
+    };
+
+    // to show all the markers
+    this.setAllShow = function(marker) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].show(marker);
+            markers[i].setVisible(marker);
+        }
+    };
+
+};
